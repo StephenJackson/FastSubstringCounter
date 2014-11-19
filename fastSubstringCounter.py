3c433a1f1b173ac94342f48fb7968918169208e2
Stephen Jackson <stephen@stephenjacksononline.com>
2014-11-19 13:19:06 -0600

    Fixed errors for empty substring

--- fastSubstringCounter.py
+++ fastSubstringCounter.py
@@ -1,8 +1,11 @@
 
 # first we take the length of the input string. Then we subtract from that the length
-# of the same string without any instances of the substring.
+# of the same string without any instances of the substring
 # (we split out the substring and then rejoin the result).
 # then we divide the difference by the length of the substring.
 # the quotient is the number of times that substring occurs in input string.
-def my_count(string_in, sub_string):
-  return (len(string_in) - len("".join(string_in.split(sub_string)))) / len(sub_string)
+def substring_counter(string_in, sub_string):
+  if sub_string:
+    return (len(string_in) - len("".join(string_in.split(sub_string)))) / len(sub_string)
+  else:
+    return 0

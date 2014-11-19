28e761a272bb39cab0e294c660e5ded7317d7742
Stephen Jackson <stephen@stephenjacksononline.com>
2014-11-19 13:10:48 -0600

    Initial Commit

---
+++ fastSubstringCounter.py
@@ -0,0 +1,8 @@
+
+# first we take the length of the input string. Then we subtract from that the length
+# of the same string without any instances of the substring.
+# (we split out the substring and then rejoin the result).
+# then we divide the difference by the length of the substring.
+# the quotient is the number of times that substring occurs in input string.
+def my_count(string_in, sub_string):
+  return (len(string_in) - len("".join(string_in.split(sub_string)))) / len(sub_string)
